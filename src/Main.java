8160156ecb9f2ea6cdb9ef8158efda7021d85947
krmnino <kurt.manrique.n@gmail.com>
2020-09-05 21:10:47 -0400

    started integrating the graphing utility with the main program

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class Main {
 	
 	public static void main(String[] args) {
+		/*
 		int[][] adj_mtrx = {{0,0,0,1,0},{0,0,0,1,1},{0,0,0,0,0},{1,1,0,0,1},{0,1,0,1,0}};
 		int[][] adj_mtrx2 = {	{0,1,0,0,0},
 								{1,0,1,0,0},
@@ -12,10 +13,10 @@ public class Main {
 								{0,0,1,0,1},
 								{0,0,0,1,0}};
 		//0-1-2-3-4
-		Graph test = new Graph(adj_mtrx2);
-		test.largest_connected_bfs(5);
+		Graph test = new Graph(adj_mtrx);
+		System.out.println(test.largest_connected_bfs(3));
+		*/
 		
-		/*
 		int n = 40;
 		double p;
 		double[] c = {0.2, 0.4, 0.6, 0.8, 1.0, 1.2,	1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0};
@@ -31,8 +32,9 @@ public class Main {
 			}
 			System.out.println(c[i] + " - " + threshold_met / 500.0);
 		}
+		
 		try {
-			ProcessBuilder builder = new ProcessBuilder("python");
+			ProcessBuilder builder = new ProcessBuilder("python graphing_util.py");
 			Process proc = builder.start();
 			BufferedReader r = new BufferedReader(new InputStreamReader(proc.getInputStream()));
 	        String line;
@@ -42,9 +44,7 @@ public class Main {
 	            System.out.println(line);
 	        }
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
 	}
 }
